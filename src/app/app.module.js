a621cc01672928480ae0786001092e2d32042ffa
Caique Caleiro <caiquecaleiro@gmail.com>
2016-09-30 16:32:12 -0300

    Add initial project structure

---
+++ src/app/app.module.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import angular from 'angular';
+
+angular
+  .module('app', []);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/src/index.html',
   filename: 'index.html',
@@ -14,12 +15,21 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: 
-    [{
-   		test: /\.js$/,
-			loader: 'babel-loader',
-			exclude: /node_modules/
-		}]
+    loaders: [
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   plugins: [HTMLWebpackPluginConfig]
 };
