b7b6ccd71d798ad7c3b2036c7a486cc2acde5723
Caique Caleiro <caiquecaleiro@gmail.com>
2016-10-05 21:08:39 -0300

    Remove old modules

--- src/app/about/about.module.js
+++
@@ -1,9 +0,0 @@
-import AboutController from './about.controller';
-import AboutService from './about.service';
-import configRoute from './config.route';
-
-export default angular
-  .module('app.about', [])
-  .controller('AboutController', AboutController)
-  .service('AboutService', AboutService)
-  .config(configRoute);
--- src/app/mainPanel/mainPanel.module.js
+++
@@ -1,7 +0,0 @@
-import MainPanelController from './mainPanel.controller';
-import configRoute from './config.route';
-
-export default angular
-  .module('app.mainPanel', [])
-  .config(configRoute)
-  .controller('MainPanelController', MainPanelController);
